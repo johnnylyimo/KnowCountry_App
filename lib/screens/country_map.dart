0d9e3c85008e32715b8a58f7791538d89fbf496c
johnnylyimo <johnnylyimo@gmail.com>
2020-01-26 20:51:08 +0300

    Created CountryMap Stateful widget

--- lib/screens/country_map.dart
+++ lib/screens/country_map.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class CountryMap extends StatefulWidget {
+  @override
+  _CountryMapState createState() => _CountryMapState();
+}
+
+class _CountryMapState extends State<CountryMap> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
