e6ef9aedf93dcd07e05ab5cf392654119bc19f91
johnnylyimo <johnnylyimo@gmail.com>
2020-01-26 20:40:02 +0300

    Added FlipCard 4 to GridView children

--- lib/screens/country.dart
+++ lib/screens/country.dart
@@ -58,7 +58,8 @@ class Country extends StatelessWidget {
                       ),
                     ),
                   ),
-                )
+                ),
+                FlipCard()
               ],
             );
           }),
