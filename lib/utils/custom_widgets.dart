b48141a552430d26dfc0bf0849edaf4745b49547
johnnylyimo <johnnylyimo@gmail.com>
2020-01-26 19:35:44 +0300

    Updated return type to Card

--- lib/utils/custom_widgets.dart
+++ lib/utils/custom_widgets.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class BackCountryInfoCard extends StatelessWidget {
   final String data;
   BackCountryInfoCard({this.data});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card();
   }
 }
