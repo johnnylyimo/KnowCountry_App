2f146f34681120244f7e1f8f0518637ab845a049
johnnylyimo <johnnylyimo@gmail.com>
2020-01-26 15:58:26 +0300

    Added Project Starter files

---
+++ android/app/src/main/java/molystechie/search_country/MainActivity.java
@@ -0,0 +1,13 @@
+package molystechie.search_country;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(KnowCountry());
+
+class KnowCountry extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Search Country App',
+      theme: ThemeData(),
+    );
+  }
+}
