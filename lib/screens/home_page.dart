ad1ad6b4a41b61fc9dee01716caab538fc7988f7
johnnylyimo <johnnylyimo@gmail.com>
2020-01-26 16:20:32 +0300

    Created Stateful widget AllCountriesHomePage

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class AllCountriesHomePage extends StatefulWidget {
+  @override
+  _AllCountriesHomePageState createState() => _AllCountriesHomePageState();
+}
+
+class _AllCountriesHomePageState extends State<AllCountriesHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
