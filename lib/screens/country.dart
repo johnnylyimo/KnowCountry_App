8154e82a2a4cd440febf4c4bb7ddb7bb2f1171b6
johnnylyimo <johnnylyimo@gmail.com>
2020-01-26 19:52:36 +0300

    Defined Stateless class Constructor

--- lib/screens/country.dart
+++ lib/screens/country.dart
@@ -3,6 +3,7 @@ import 'package:search_country/utils/custom_widgets.dart';
 
 class Country extends StatelessWidget {
   final Map country;
+  Country(this.country);
 
   @override
   Widget build(BuildContext context) {
