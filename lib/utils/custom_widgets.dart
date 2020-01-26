183118cd2b46ac47c554453f5d1f87c06f341513
johnnylyimo <johnnylyimo@gmail.com>
2020-01-26 19:34:33 +0300

    Defined State variable data

--- lib/utils/custom_widgets.dart
+++ lib/utils/custom_widgets.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class BackCountryInfoCard extends StatelessWidget {
+  final String data;
   @override
   Widget build(BuildContext context) {
     return Container();
