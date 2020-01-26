19083ef5a608dc15786b916094155ffeddc33f35
johnnylyimo <johnnylyimo@gmail.com>
2020-01-26 20:58:09 +0300

    Defined State Constructor

--- lib/screens/country_map.dart
+++ lib/screens/country_map.dart
@@ -3,15 +3,15 @@ import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class CountryMap extends StatefulWidget {
+  String countryName;
+  List latlng;
+  CountryMap(this.countryName, this.latlng);
+
   @override
   _CountryMapState createState() => _CountryMapState();
 }
 
 class _CountryMapState extends State<CountryMap> {
-  String countryName;
-  List latlng;
-
-
   @override
   Widget build(BuildContext context) {
     return Container();
