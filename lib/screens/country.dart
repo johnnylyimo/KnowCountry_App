07c88617a16cc735e2826d76a98a9beb4c09f000
johnnylyimo <johnnylyimo@gmail.com>
2020-01-26 19:51:45 +0300

    Defined Stateless class variable country

--- lib/screens/country.dart
+++ lib/screens/country.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:search_country/utils/custom_widgets.dart';
 
 class Country extends StatelessWidget {
+  final Map country;
+
   @override
   Widget build(BuildContext context) {
     return Container();
