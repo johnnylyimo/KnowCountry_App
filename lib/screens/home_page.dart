931c39cac00df9b541b3949065a65469be694781
johnnylyimo <johnnylyimo@gmail.com>
2020-01-26 18:50:39 +0300

    Defined icon to IconButton

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -40,7 +40,7 @@ class _AllCountriesHomePageState extends State<AllCountriesHomePage> {
                 ),
               ),
         actions: <Widget>[
-          isSearching ? IconButton()
+          isSearching ? IconButton(icon: Icon(Icons.cancel),)
         ],
       ),
     );
