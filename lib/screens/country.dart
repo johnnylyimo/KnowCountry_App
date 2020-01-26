1c18e302d66c6b627b04595aa604ec8001e00b5b
johnnylyimo <johnnylyimo@gmail.com>
2020-01-26 19:50:40 +0300

    Created Country Stateless widget

--- lib/screens/country.dart
+++ lib/screens/country.dart
@@ -1,2 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:search_country/utils/custom_widgets.dart';
+
+class Country extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
