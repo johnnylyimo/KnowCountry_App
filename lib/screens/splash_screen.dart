974150a93b7a08a97b4cba3bd65b901cefc24653
johnnylyimo <johnnylyimo@gmail.com>
2020-01-27 04:07:23 +0300

    Defined Column MainAxisAlignment.spaceEvenly

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -24,7 +24,9 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      ),
     );
   }
 }
