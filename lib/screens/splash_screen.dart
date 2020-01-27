c9a42f116f882160f2a0395d4ce05edec1fcbc19
johnnylyimo <johnnylyimo@gmail.com>
2020-01-27 04:04:17 +0300

    Defined Navigator to go to the next screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -12,7 +12,13 @@ class _SplashScreenState extends State<SplashScreen> {
     // TODO: implement initState
     super.initState();
 
-    Future.delayed(Duration(seconds: 3), () {});
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => AllCountriesHomePage(),
+          ));
+    });
   }
 
   @override
