ad86de228d1f46fb541244f47fb98caee5d671aa
johnnylyimo <johnnylyimo@gmail.com>
2020-01-27 03:47:00 +0300

    Added Google Map API to the IOS app

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,10 +1,12 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [GMSServices provideAPIKey:@"AIzaSyBdpBOHGMJmSmPE3R9tyZMDy9D3uFMuVmk"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
